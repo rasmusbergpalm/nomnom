239cba4935afbad2f6f141fa3790c263fb1eacae
rasmuspalm <rasmuspalm@7190afe4-8e12-4883-a223-76ebe4e6771f>
2011-08-21 23:09:17 +0000

    added templates again...

--- app/config/database.php
+++ app/config/database.php
@@ -6,7 +6,7 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'a32',
+		'password' => '',
 		'database' => 'nomnom',
 		'prefix' => '',
 	);
